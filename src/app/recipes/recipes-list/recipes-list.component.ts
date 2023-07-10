9706164ab073c3990817f9ba380d0c92f4f5b6b4
aparvare <amir-hossein.parvaresh@capgemini.com>
2023-07-10 11:20:39 +0200

    Outputting list of recipes with ngFor

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -8,7 +8,8 @@ import {Recipe} from "../recipe.model";
 })
 export class RecipesListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A test recipe', 'This is simple recipe', 'https://assets.bonappetit.com/photos/61b775620fb3fcc4cbf036c1/1:1/w_1920,c_limit/20211208%20Spaghetti%20Squash%20with%20Tomato%20Sauce%20and%20Mozarella%20LEDE.jpg')
+    new Recipe('A test recipe', 'This is simple recipe', 'https://assets.bonappetit.com/photos/61b775620fb3fcc4cbf036c1/1:1/w_1920,c_limit/20211208%20Spaghetti%20Squash%20with%20Tomato%20Sauce%20and%20Mozarella%20LEDE.jpg'),
+    new Recipe('Pasta recipe', 'This is a delicious pasta', 'https://cookieandkate.com/images/2018/08/crisp-apple-kohlrabi-salad-recipe-1-768x1151.jpg')
   ];
 
   ngOnInit() {
